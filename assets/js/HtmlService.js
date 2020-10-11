9ccd2491e36fd48808dfa7b90a0dae4d3dc33ac9
Iury <iurymig.sht@gmail.com>
2020-10-11 15:44:23 -0300

    criando form para add/edit carro

--- assets/js/HtmlService.js
+++ assets/js/HtmlService.js
@@ -7,12 +7,14 @@ export default class HtmlService {
     this.bindFormEvent();
     this.listCars();
     const btnAdd = document.getElementById("add-car");
-    btnAdd.addEventListener('click', () => console.log("HAHAHA"));
+    if (btnAdd) {
+      btnAdd.addEventListener('click', () => console.log("HAHAHA"));
+    }
   }
 
   bindFormEvent() {
     const form = document.querySelector('form');
-    form.addEventListener('submit', event => {
+    form?.addEventListener('submit', event => {
       event.preventDefault();
       this.addCar(form.item.value);
       form.reset();
@@ -38,33 +40,33 @@ export default class HtmlService {
   }
 
   toggleCar(li) {
-    const carId = +li.getAttribute('data-item-id');
-    li.classList.toggle(DONE);
-    const isDone = li.classList.contains(DONE);
-    this.saveCar(carId, isDone);
+    window.location.href = './carForm.html';
+    // const carId = +li?.getAttribute('data-item-id');
+    // li?.classList.toggle(DONE);
+    // const isDone = li?.classList.contains(DONE);
+    // this.saveCar(carId, isDone);
   }
 
   async deleteCar(li) {
-    const carId = +li.getAttribute('data-item-id');
+    const carId = +li?.getAttribute('data-item-id');
     await this.carService.delete(carId);
-    li.remove();
+    li?.remove();
   }
 
   addToHtmlList(car) {
     const ul = document.querySelector('ul');
     const li = document.createElement('li');
-    // const span = document.createElement('span');
     const nameSpan = document.createElement('span');
     const manufacturerSpan = document.createElement('span');
     const colorSpan = document.createElement('span');
     const yearSpan = document.createElement('span');
     const button = document.createElement('button');
 
-    li.setAttribute('data-item-id', car.id);
-    li.addEventListener('click', () => this.toggleCar(li));
+    li?.setAttribute('data-item-id', car.id);
+    li?.addEventListener('click', () => this.toggleCar(li));
 
 
-    nameSpan.textContent = `${car.name}`;
+    nameSpan.textContent = `${car.id} ${car.name}`;
     manufacturerSpan.textContent = `${car.manufacturer}`;
     colorSpan.textContent = `${car.color}`;
     yearSpan.textContent = `${car.year}`;
@@ -76,14 +78,14 @@ export default class HtmlService {
     });
 
     if (car.done) {
-      li.classList.add(DONE);
+      li?.classList.add(DONE);
     }
 
-    li.appendChild(nameSpan);
-    li.appendChild(manufacturerSpan);
-    li.appendChild(colorSpan);
-    li.appendChild(yearSpan);
-    li.appendChild(button);
-    ul.appendChild(li);
+    li?.appendChild(nameSpan);
+    li?.appendChild(manufacturerSpan);
+    li?.appendChild(colorSpan);
+    li?.appendChild(yearSpan);
+    li?.appendChild(button);
+    ul?.appendChild(li);
   }
 }
